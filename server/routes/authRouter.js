5e5f1cec4b54a6d92a552e561e536e3197352f07
vicky <vicky@localhost.localdomain>
2024-06-04 05:19:34 +0530

    hi

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Signvalidate, Logvalidate } = require("../controllers/validateform");
+const { Signvalidate, Logvalidate } = require("./controllers/validateform");
 const pool = require("../dataBase");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
